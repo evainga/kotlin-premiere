4f9ad5e928866b17208b7bbb8e224162189a0861
Eva Goettert <ego87@web.de>
2019-03-31 11:48:24 +0200

    First commit

---
+++ src/main/kotlin/de/kotlinpremiere/KotlinPremiereApplication.kt
@@ -0,0 +1,12 @@
+package de.kotlinpremiere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPremiereApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPremiereApplication>(*args)
+}
+
